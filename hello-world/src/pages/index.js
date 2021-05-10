9053f8deb2ea0a9a431b7134f65effb5947fc089
Nick (Git client) <nick@cassiel.com>
2021-05-10 18:04:25 +0100

    Tutorial done.

--- hello-world/src/pages/index.js
+++ hello-world/src/pages/index.js
@@ -1,5 +1,69 @@
 import React from "react"
-
-export default function Home() {
-  return <div>Hello world!</div>
+//import { css } from "@emotion/react"
+import { Link, graphql } from "gatsby"
+export default function Home({ data }) {
+  return (
+      <div>
+        <h1
+        /*
+          css={css`
+            display: inline-block;
+            border-bottom: 1px solid;
+          `}*/
+        >
+         Index Title
+        </h1>
+        <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+        {data.allMarkdownRemark.edges.map(({ node }) => (
+          <div key={node.id}>
+            <Link
+              to={node.fields.slug}
+              /*
+              css={css`
+                text-decoration: none;
+                color: inherit;
+              `}*/
+            >
+              <h3
+              /*
+                css={css`
+                  margin-bottom: ${rhythm(1 / 4)};
+                `}*/
+              >
+                {node.frontmatter.title}{" "}
+                <span
+                /*
+                  css={css`
+                    color: #555;
+                  `}*/
+                >
+                  — {node.frontmatter.date}
+                </span>
+              </h3>
+              <p>{node.excerpt}</p>
+            </Link>
+          </div>
+        ))}
+      </div>
+  )
 }
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          fields {
+            slug
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
